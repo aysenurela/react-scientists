ad838ba06b876f963f80bcbfec57ae901877aedf
manashishah <manashishah5@gmail.com>
2024-02-20 17:12:59 -0500

    scientists project submission

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,11 @@
-// Card component takes any children
-// renders children inside div wrapper with className 'Card'
-// if there is no children component should return null;
+export default function Card({ children }) {
+    if (!children) {
+        return null;
+    }
 
-export default function Card() {}
+    return (
+        <div className="Card">
+            {children}
+        </div>
+    );
+}
--- src/components/PersonDetails.js
+++ src/components/PersonDetails.js
@@ -1,6 +1,11 @@
-// Component PersonDetails should render an unordered list 
-  // unordered list should have two <li> item with below details:
-    // 1. Person's profession 
-    // 2. Person's accomplishment
+export default function PersonDetails({ profession, accomplishment }) {
+  return (
+    <div>
+      <ul>
+        <li>{profession}</li>
+        <li>{accomplishment}</li>
+      </ul>
+    </div>
 
-export default function PersonDetails({ profession, accomplishment }) {}
+  );
+}
