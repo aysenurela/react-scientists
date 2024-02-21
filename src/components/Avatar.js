fbebc2a31e35a863fff116a27f8368a3ad03638f
aayushg07 <aayushgautam.ag@gmail.com>
2024-02-21 16:45:59 -0500

    Scientist Project- Aayush Gautam

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -2,8 +2,16 @@
 // returns an img element with className 'Avatar'
 // img element should have src and alt attributes
 
-export default function Avatar(props) {    
+export default function Avatar(props) {
     const { person } = props;
 
-    // return ...
+    return (
+        <div>
+            <img
+                className="Avatar"
+                src={person.imageUrl}
+                alt="Images"
+            ></img>
+        </div>
+    );
 }
--- src/components/PersonDetails.js
+++ src/components/PersonDetails.js
@@ -1,6 +1,15 @@
 // Component PersonDetails should render an unordered list 
-  // unordered list should have two <li> item with below details:
-    // 1. Person's profession 
-    // 2. Person's accomplishment
+// unordered list should have two <li> item with below details:
+// 1. Person's profession 
+// 2. Person's accomplishment
 
-export default function PersonDetails({ profession, accomplishment }) {}
+export default function PersonDetails({ profession, accomplishment }) {
+  return (
+    <div>
+      <ul>
+        <li>{profession}</li>
+        <li>{accomplishment}</li>
+      </ul>
+    </div>
+  );
+}
