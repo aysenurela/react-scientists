e9f36668f42b04eaf1cac91fcb13bf0a6696560d
Ujjwal Ahuja <101486748@georgebrown.ca>
2024-02-21 17:12:38 -0500

    Ujjwal Ahuja

--- src/components/PersonDetails.js
+++ src/components/PersonDetails.js
@@ -3,4 +3,13 @@
     // 1. Person's profession 
     // 2. Person's accomplishment
 
-export default function PersonDetails({ profession, accomplishment }) {}
+export default function PersonDetails({ profession, accomplishment }) {
+  return (
+    <div>
+      <ul>
+        <li>{profession}</li>
+        <li>{accomplishment}</li>
+      </ul>
+    </div>
+  );
+}
