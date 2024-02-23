58eb4a92b90b9e27204682f6a607318836fc856a
srishtiisharmaa <srishtisharma2004@gmail.com>
2024-02-23 14:15:21 -0500

    Updated Avatar, Card, and PersonDetails components

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -2,8 +2,9 @@
 // returns an img element with className 'Avatar'
 // img element should have src and alt attributes
 
-export default function Avatar(props) {    
-    const { person } = props;
-
-    // return ...
-}
+export default function Avatar({ person }) {    
+    return (
+        <img src={my-avatar.jpg} alt={srishti.sharma} className="Avatar" />
+    );
+  }
+  
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,18 @@
+import React from 'react';
+import YourImage from '.my-avatar.jpg'; // Update the path accordingly
+
 // Card component takes any children
 // renders children inside div wrapper with className 'Card'
 // if there is no children component should return null;
 
-export default function Card() {}
+export default function Card({ children }) {
+    return (
+        <div className="Card">
+            <img src={YourImage} alt="Your Image" className="Card-image" />
+            <PersonDetails profession="UI/UX designer" accomplishment="Designed a website" />
+            <div className="Card-content">
+                {children || null}
+            </div>
+        </div>
+    );
+}
